2f9a2fd47ad72e33ffbb5049088f2a5b129c0fc9
BraulioArione <115657222+BraulioArione@users.noreply.github.com>
2025-04-22 01:17:35 +0200

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,26 @@
-output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+output "aks_cluster_id" {
+  description = "The ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.devopsshack_aks.id
 }
 
-output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+output "aks_kube_config" {
+  description = "Kube config to connect to the AKS cluster"
+  value       = azurerm_kubernetes_cluster.devopsshack_aks.kube_config_raw
+  sensitive   = true
 }
 
-output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.devopsshack_rg.name
+}
+
+output "vnet_id" {
+  description = "The ID of the Virtual Network"
+  value       = azurerm_virtual_network.devopsshack_vnet.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  description = "The IDs of the subnets"
+  value       = [for subnet in azurerm_subnet.devopsshack_subnet : subnet.id]
 }
+
